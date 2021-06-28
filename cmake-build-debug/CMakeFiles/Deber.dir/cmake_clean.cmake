e8a8eb1b8be1bd8a1f4e6edeefbb78555f80d15e
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-06-28 20:18:52 +1200

    Deber cubico

---
+++ cmake-build-debug/CMakeFiles/Deber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deber.dir/main.cpp.obj"
+  "Deber.exe"
+  "Deber.exe.manifest"
+  "Deber.pdb"
+  "libDeber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Deber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
